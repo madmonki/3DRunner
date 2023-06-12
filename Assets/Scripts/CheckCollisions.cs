e669cda5f173be31ae5f4389151bc82935241523
madmonki <90353885+madmonki@users.noreply.github.com>
2023-06-12 21:29:44 +0300

    CoinCollection_ScoreAdded

---
+++ Assets/Scripts/CheckCollisions.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CheckCollisions : MonoBehaviour
+{
+    public int score;
+    public TextMeshProUGUI CoinText;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            AddCoin();
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    public void AddCoin()
+    {
+        ++score;
+        CoinText.text = "Score: " + score.ToString();
+    }
+}
